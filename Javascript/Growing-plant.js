a2bdd94b4dc8e52025edb616814258751bdcceeb
Ramesh Naidu Allu <49170790+rna@users.noreply.github.com>
2019-12-05 20:24:37 +0530

    Update Growing-plant.js

--- Javascript/Growing-plant.js
+++ Javascript/Growing-plant.js
@@ -1,3 +1,15 @@
+/*
+Problem:
+Each day a plant is growing by upSpeed meters. Each night that plant's height decreases by downSpeed meters due to the lack of sun heat. Initially, plant is 0 meters tall. We plant the seed at the beginning of a day. We want to know when the height of the plant will reach a certain level.
+
+Examples:
+For upSpeed = 100, downSpeed = 10 and desiredHeight = 910, the output should be 10.
+
+For upSpeed = 10, downSpeed = 9 and desiredHeight = 4, the output should be 1.
+
+
+*/
+
 //Abel's solution
 function growingPlant(upSpeed, downSpeed, desiredHeight) {
   //coding and coding..
@@ -11,6 +23,19 @@ function growingPlant(upSpeed, downSpeed, desiredHeight) {
     currentHeight -= downSpeed;
     if(currentHeight == desiredHeight){
     return days;}
+  }
+  return days;
+}
+
+//Ramesh's Solution
+function growingPlant(upSpeed, downSpeed, desiredHeight) {
+  //coding and coding..
+  var days = 1;
+  var height = upSpeed;
+  while(height<desiredHeight){
+    height += upSpeed;
+    height -= downSpeed;
+    days++;
   }
   return days;
 }
